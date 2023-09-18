bd41d0dc7f8a6b1471bc79ade6a7c5457358ae39
niceu <niceusantos1365@gmail.com>
2023-09-18 12:51:38 -0300

    Add project files.

---
+++ execicio/Data/execicioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using execicio.Models;
+
+namespace execicio.Data
+{
+    public class execicioContext : DbContext
+    {
+        public execicioContext (DbContextOptions<execicioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<execicio.Models.Categoria> Categoria { get; set; } = default!;
+    }
+}
